8ad8bacbe1f68864f58fea5ca8ecbda4c333bc33
Ko3ch <tomko3ch@gmail.com>
2020-06-14 17:28:47 +0300

    added application modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { DiaryComponent } from './diary/diary.component';
+import { DiaryEntryComponent } from './diary/diary-events/diary-entry/diary-entry.component';
+import { DiaryPreviewComponent } from './diary/diary-events/diary-preview/diary-preview.component';
+import { DiaryEventsComponent } from './diary/diary-events/diary-events.component';
+import { DiaryFormComponent } from './diary/diary-events/diary-form/diary-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DiaryComponent,
+    DiaryEntryComponent,
+    DiaryPreviewComponent,
+    DiaryEventsComponent,
+    DiaryFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
